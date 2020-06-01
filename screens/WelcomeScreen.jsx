f737b20c88db1347f9cbfbedf1b3f89c750ed2f4
vm947 <vm947@nyu.edu>
2020-06-01 16:25:31 +0300

    changes

---
+++ screens/WelcomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {View, ImageBackground, Text, StyleSheet} from 'react-native';
+
+export const WelcomeScreen = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
