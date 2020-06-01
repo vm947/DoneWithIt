d885a369734c1685134737d97c422703497faaeb
vm947 <vm947@nyu.edu>
2020-06-01 14:00:27 +0300

    changes

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  Dimensions,
   StyleSheet,
   Text,
   TouchableNativeFeedback,
@@ -11,28 +12,38 @@ import {
   StatusBar,
   Platform,
 } from "react-native";
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
 
 export default function App() {
+  const { landscape } = useDeviceOrientation();
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Button
-        color="purple"
-        title="Click Me"
-        onPress={() =>
-          Alert.alert("My title", "My message", [
-            { text: "yes", onPress: () => console.log("Yes") },
-            { text: "no", onPress: () => console.log("no") },
-          ])
-        }
-      />
-    </SafeAreaView>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: 'row'
+      }}
+    >
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: 'gold',
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: 'tomato',
+        width: 100,
+        height: 100
+      }}/>
+
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "dodgerblue",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
