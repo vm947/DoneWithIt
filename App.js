c63308479800876b20ad389f3daa3120830f1116
vm947 <vm947@nyu.edu>
2020-06-02 11:12:30 +0300

    changes

--- App.js
+++ App.js
@@ -1,65 +1,8 @@
 import React from "react";
-import {
-  Dimensions,
-  StyleSheet,
-  Text,
-  TouchableNativeFeedback,
-  Image,
-  View,
-  SafeAreaView,
-  Alert,
-  Button,
-  StatusBar,
-  Platform,
-} from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        // flexWrap: 'wrap',
-        // alignContent: 'center'
-        //alignItems aligns in the LINE
-        //alignContent works only with flexWrap
-        //alignContent works based on page
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-          position: 'relative'
-          //all components have default relative position
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.backGround}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      </View>
+        <Text style={styles.text}>Sell What You Don't Need</Text>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  backGround: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    top: 70,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: 'center',
+  },
+  text:{
+    position: "absolute",
+    bottom: 400
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
+
+export default WelcomeScreen;
