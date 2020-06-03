995d778830d3dd650f68656ce9fadb8f5b8cdfd7
vm947 <vm947@nyu.edu>
2020-06-03 09:38:04 +0300

    changes

--- App.js
+++ App.js
@@ -2,12 +2,23 @@ import React from "react";
 import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import Borders from './practice/Borders';
-import Shadows from './practice/Shadows';
-import PaddingAndMargin from './practice/PaddingAndMargin';
-import StyleText from './practice/StyleText';
-import Icons from './practice/Icons';
+import Borders from "./practice/Borders";
+import Shadows from "./practice/Shadows";
+import PaddingAndMargin from "./practice/PaddingAndMargin";
+import StyleText from "./practice/StyleText";
+import Icons from "./practice/Icons";
+import AppText from "./componenets/AppText";
 
 export default function App() {
-  return <Icons />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppText>I Love React</AppText>
+    </View>
+  );
 }
