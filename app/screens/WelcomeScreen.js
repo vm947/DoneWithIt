b0b42bb8ca96a97ecc91132d1a43289bd025b846
vm947 <vm947@nyu.edu>
2020-06-04 11:31:23 +0300

    changes

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -5,13 +5,14 @@ import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={2}
       style={styles.backGround}
       source={require("../assets/background.jpg")}
     >
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require("../assets/logo-red.png")} />
       </View>
-      <Text style={styles.text}>Sell What You Don't Need</Text>
+      <Text style={styles.tagline}>Sell What You Don't Need</Text>
       <View style={styles.buttonContainer}>
         <AppButton title="Login" />
         <AppButton title="Register" color="secondary" />
@@ -41,7 +42,9 @@ const styles = StyleSheet.create({
     top: 70,
     alignItems: "center",
   },
-  text: {
+  tagline: {
+    fontSize: 25,
+    fontWeight: "bold",
     position: "absolute",
     bottom: 400,
   },
