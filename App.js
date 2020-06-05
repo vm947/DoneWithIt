c73bfb503756c34f5badcdb9288d7221cce9e2e6
vm947 <vm947@nyu.edu>
2020-06-05 16:58:07 +0300

    changes

--- App.js
+++ App.js
@@ -5,9 +5,11 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./componenets/AppButton";
 import Card from "./componenets/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen"
+
 
 export default function App() {
   return (
-    <ViewImageScreen/>
+    <MessagesScreen/>
   );
 }
