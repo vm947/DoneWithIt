b17b0106c6c69f1715f8d97dbb0c0df02f4a20f6
vm947 <vm947@nyu.edu>
2020-06-08 09:52:16 +0300

    changes

--- componenets/PickerItem.js
+++ componenets/PickerItem.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React from "react";
+import { TouchableOpacity, StyleSheet } from "react-native";
 import AppText from "./AppText";
 
-
-function PickerItem({label, onPress}) {
-    return (
-        <TouchableOpacity onPress={onPress} >
-            <AppText>{label}</AppText>
-        </TouchableOpacity>
-    );
+function PickerItem({ label, onPress }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <AppText style={styles.text}>{label}</AppText>
+    </TouchableOpacity>
+  );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    padding: 20,
+  },
+});
 
 export default PickerItem;
